1192c1cae9e78b77ef34945323a05aca605b974d
agnesp24 <agnes.beatriz01@gmail.com>
2024-12-11 14:43:41 -0300

    Add funções

--- Coleta_web.py
+++ Coleta_web.py
@@ -1,20 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://en.wikipedia.org/wiki/Python"
-requisicao = requests.get(url)
-extracao = BeautifulSoup(requisicao.text, "html.parser")
-
 #Exibir o texto do site
 #print(extracao.text.strip()) #Strip remove os espaços em branco
 
 
-#Filtrar a exibição pela tag
+#Função para filtrar a exibição pela tag
+def filtro_tags():
+    url = "https://wiki.python.org.br/AprendaMais"
+    requisicao = requests.get(url)
+    extracao = BeautifulSoup(requisicao.text, "html.parser")
 
-conta_h2 = 0
+    conta_h2 = 0
+    conta_p = 0
 
-for linha_texto in extracao.find_all("h2"):
-    if (linha_texto.name == "h2"):
-        titulo = (linha_texto.text.strip())
-        conta_h2 += 1
-    print(conta_h2, titulo)
+    for linha_texto in extracao.find_all(["h2","p"]):
+        if linha_texto.name == "h2":
+            titulo = (linha_texto.text.strip())
+            conta_h2 += 1
+            print("Título: ", conta_h2, titulo)
+        elif linha_texto.name == "p":
+            conta_p += 1
+            paragrafo = (linha_texto.text.strip())
+            print(conta_p, paragrafo)
+
+
+#Função: Extraindo links(a) de dentro de parágrafos(p), que estão dentro de títulos(h2).
+def extrair_links():
+    url = "https://wiki.python.org.br/AprendaMais"
+    requisicao = requests.get(url)
+    extracao = BeautifulSoup(requisicao.text, "html.parser")
+
+    for titulo in extracao.find_all("h2"):
+        print("Título: ", titulo.text.strip())
+        for link in titulo.find_next_siblings("p"):
+            for a in link.find_all("a", href=True):
+                print("Link: ", a.text.strip(), "| URL: ", a["href"])
+
+#Módulo principal
+extrair_links()
